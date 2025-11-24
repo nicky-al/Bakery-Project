81a52c2e0de03ab0370f7a5f8d3e219192a4ff5f
nicky <alam.nicky05@gmail.com>
2025-11-24 18:12:15 -0500

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+</main>
+<footer class="site-footer">
+  <div class="container">
+    <p>&copy; <?php echo date('Y'); ?> Sweets & Crumbs Bakery. All rights reserved.</p>
+  </div>
+</footer>
+    <div id="toast"></div>
+
+<script src="/assets/js/app.js?v=<?php echo time(); ?>" defer></script>
+</body>
+</html>
